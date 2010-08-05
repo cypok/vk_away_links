5a4ecc1dcfd8a00861f31ad926965788a0d067ef
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-08-05 13:31:36 +0700

    Return support of old algorithm (changing document.location)
    used when links are loaded via ajax

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+function replace_away_link_at(obj) {
+  if(!obj.href) return;
+
+  if( obj.href.match(/^http:\/\/vkontakte\.ru\/away\.php/) ) {
+    obj.href = unescape(obj.href.substr(32));
+  }
+  if( obj.href.match(/^http:\/\/vk\.com\/away\.php/) ) {
+    obj.href = unescape(obj.href.substr(26));
+  }
+}
+
+replace_away_link_at(document.location);
+
 links = document.links;
 for(var i = 0; i < links.length; ++i) {
-  href = links[i].href;
-  if(href) {
-    if(href.match(/http:\/\/vkontakte\.ru\/away\.php\?to=/)) {
-      links[i].href = unescape(href.substr(32));
-    }
-    if(href.match(/http:\/\/vk\.com\/away\.php\?to=/)) {
-      links[i].href = unescape(href.substr(26));
-    }
-  }
+  replace_away_link_at(links[i]);
 }
