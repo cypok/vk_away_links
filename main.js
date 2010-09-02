9941a50228016627578bf61a5199612fce462306
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-09-03 00:34:50 +0700

    More accurate url cropping

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 function replace_away_link_at(obj) {
   if(!obj.href) return;
 
-  if( obj.href.match(/^http:\/\/vkontakte\.ru\/away\.php/) ) {
-    obj.href = unescape(obj.href.substr(32));
-  }
-  if( obj.href.match(/^http:\/\/vk\.com\/away\.php/) ) {
-    obj.href = unescape(obj.href.substr(26));
+  match = obj.href.match(/^http:\/\/vkontakte\.ru\/away\.php\?to=([^&]+)/) ||
+          obj.href.match(/^http:\/\/vk\.com\/away\.php\?to=([^&]+)/);
+  if( match ) {
+    obj.href = unescape(match[1]);
   }
 }
 
